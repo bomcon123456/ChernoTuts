d9b53f6d92c6f96ef219c9b2156100560d8fd26d
Trung <bomcon123456@gmail.com>
2018-07-18 16:43:48 +0700

    TestMenu

--- ChernoTuts/src/Application.cpp
+++ ChernoTuts/src/Application.cpp
@@ -63,23 +63,43 @@ int main(void)
 		ImGui::StyleColorsDark();
 
 		//						IMGUI
+		test::Test* currentTest = nullptr;
+		test::TestMenu* testMenu = new test::TestMenu(currentTest);
+		currentTest = testMenu;
 
-		test::TestClearColor test;
-
+		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
 
 		while (!glfwWindowShouldClose(window))
 		{
+			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			renderer.Clear();
-			test.OnUpdate(0.0f);
-			test.OnRender();
 
 			ImGui_ImplGlfwGL3_NewFrame();
-			test.OnImGuiRender();
+
+			if (currentTest)
+			{
+				currentTest->OnUpdate(0.0f);
+				currentTest->OnRender();
+				ImGui::Begin("Test");
+				if (currentTest != testMenu && ImGui::Button("<-"))
+				{
+					delete currentTest;
+					currentTest = testMenu;
+				}
+				currentTest->OnImGuiRender();
+				ImGui::End();
+			}
+
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
+		}
+		delete currentTest;
+		if (currentTest != testMenu)
+		{
+			delete testMenu;
 		}
 	}
 	ImGui_ImplGlfwGL3_Shutdown();
--- ChernoTuts/src/tests/Test.h
+++ ChernoTuts/src/tests/Test.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <functional>
+#include <vector>
+
 namespace test 
 {
 	class Test 
@@ -10,5 +13,26 @@ namespace test
 		virtual void OnUpdate(float deltaTime) {}
 		virtual void OnRender() {}
 		virtual void OnImGuiRender() {}
+	};
+	class TestMenu : public Test
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+
+		void OnImGuiRender() override;
+		
+		template<typename T>
+		void RegisterTest(const std::string& name)
+		{
+			std::cout << "Registering test " << name << std::endl;
+
+			m_Tests.push_back(std::make_pair(name, []() { return new T; }));
+		}
+	private:
+		// An array of Test's pointer with a name
+		// The Test Object which will be pointed to will not be created initialy but created in real-time 
+		// So we must using lambda function which return Test* (std::function)
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::function<Test*()>>> m_Tests;
 	};
 }
