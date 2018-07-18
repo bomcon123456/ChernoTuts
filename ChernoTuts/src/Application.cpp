01ae00a35434235243c0fcda147e400a0d05841c
Trung <bomcon123456@gmail.com>
2018-07-18 11:03:24 +0700

    Model View Projection Matrix

--- ChernoTuts/src/Application.cpp
+++ ChernoTuts/src/Application.cpp
@@ -82,11 +82,15 @@ int main(void)
 		 * suits the [-1.0f,1.0f] axis
 		 */
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200.0f, 200.0f, 0.0f));
+
+		glm::mat4 mvp = proj * view * model;
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.1f, 0.6f, 0.9f,1.0f);
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		Texture texture("res/textures/agave.png");
 		texture.Bind(0);
