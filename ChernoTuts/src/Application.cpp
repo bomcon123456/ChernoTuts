c9e55e7be3d8144d1a4dc267e385438bf65f6630
bomcon123456 <bomcon123456@gmail.com>
2018-08-08 20:56:08 +0700

    Revert "TestMenu"
    This reverts commit d9b53f6d92c6f96ef219c9b2156100560d8fd26d.

--- ChernoTuts/src/Application.cpp
+++ ChernoTuts/src/Application.cpp
@@ -63,43 +63,23 @@ int main(void)
 		ImGui::StyleColorsDark();
 
 		//						IMGUI
-		test::Test* currentTest = nullptr;
-		test::TestMenu* testMenu = new test::TestMenu(currentTest);
-		currentTest = testMenu;
 
-		testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+		test::TestClearColor test;
+
 
 		while (!glfwWindowShouldClose(window))
 		{
-			GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 			renderer.Clear();
+			test.OnUpdate(0.0f);
+			test.OnRender();
 
 			ImGui_ImplGlfwGL3_NewFrame();
-
-			if (currentTest)
-			{
-				currentTest->OnUpdate(0.0f);
-				currentTest->OnRender();
-				ImGui::Begin("Test");
-				if (currentTest != testMenu && ImGui::Button("<-"))
-				{
-					delete currentTest;
-					currentTest = testMenu;
-				}
-				currentTest->OnImGuiRender();
-				ImGui::End();
-			}
-
+			test.OnImGuiRender();
 			ImGui::Render();
 			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 			glfwSwapBuffers(window);
 			glfwPollEvents();
-		}
-		delete currentTest;
-		if (currentTest != testMenu)
-		{
-			delete testMenu;
 		}
 	}
 	ImGui_ImplGlfwGL3_Shutdown();
